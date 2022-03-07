c637790eac562a55af1f464fe6ee0db01b8975d8
Dystian En <distian.enyus@gmail.com>
2022-03-07 17:24:12 +0700

    Feat: Login Bank BNI

--- src/component/Header/FixedTopBar.js
+++ src/component/Header/FixedTopBar.js
@@ -9,7 +9,7 @@ import {
 export const FixedTopBar = observer(() => {
     return (
         <div style={{ backgroundColor: "#CBDFFF", width: "100%", top: 0 }}>
-            <Typography.Paragraph strong style={{ textAlign: "center", color: "#145FD9", marginTop: 8, marginBottom: 8 }}>
+            <Typography.Paragraph strong style={{ textAlign: "center", color: "#145FD9", paddingTop: 4, marginBottom: 0, height: '40px' }}>
                 <FontAwesomeIcon style={{ marginRight: '1.2em' }} icon={faUnlockKeyhole} /> You’re In Secure Connection
             </Typography.Paragraph>
         </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
-
-import { AnimatedSwitch } from "react-router-transition";
+import {AnimatedSwitch} from "react-router-transition";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { Login } from "../pages/Login/Login";
 import { TermCondition } from "../pages/TermCondition";
 import { PublicRoute } from "../component/PublicRoute";
 import { PrivateRoute } from "../component/PrivateRoute";
 import { App } from "../pages/App/App";
+import {LoginBNI} from "../pages/Login/LoginBNI";
 
 // Login Bank
 import { LoginBankMandiri } from "../pages/LoginBankMandiri";
@@ -14,15 +14,16 @@ export const MainRoutes = (props) => {
     return (
         <Switch>
             <Route path="/" exact>
-                <Redirect to={"/app/home"} />
+                <Redirect to={"/app/home"}/>
             </Route>
-            <PublicRoute restricted={true} component={Login} path="/login" exact />
-            <PublicRoute restricted={true} component={TermCondition} path="/term-condition" exact />
+            <PublicRoute restricted={true} component={Login} path="/login" exact/>
+            <PublicRoute restricted={true} component={TermCondition} path="/term-condition" exact/>
 
             {/* Login Bank */}
             <PublicRoute restricted={false} component={LoginBankMandiri} path="/login/mandiri" exact />
+            <PublicRoute restricted={false} component={LoginBNI} path="/bni-login" exact/>
 
-            <PublicRoute component={App} path="/app" />
+            <PublicRoute component={App} path="/app"/>
         </Switch>
     );
 };
