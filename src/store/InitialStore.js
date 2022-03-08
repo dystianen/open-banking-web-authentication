fe5385c90810b6744998aeb45c596090cd5ea251
Daffa6633 <Artemisia512@gmail.com>
2022-03-08 11:48:08 +0700

    Feat: Refactoring Login Mandiri, Added Initial MobX.

---
+++ src/store/InitialStore.js
@@ -0,0 +1,37 @@
+import { action, observable } from "mobx";
+import { http } from "../utils/http";
+
+export class InitialStore {
+    @observable baseUrl = '/';
+    @observable data = [];
+    @observable detail = [];
+
+    constructor(context) {
+        this.context = context;
+    }
+
+    @action
+    async getAll() {
+        const res = await http.get(`${url}`);
+        return res;
+    }
+
+    @action
+    async getDetail(id) {
+        const res = await http.get(`${this.baseUrl}/${id}`);
+        return res;
+    }
+
+    @action
+    async create(data) {
+        await http.post(this.baseUrl).send(data)
+        this.getAll();
+    }
+
+    @action
+    async update(data) {
+        await http.put(this.baseUrl).send(data)
+        this.getAll();
+    }
+
+}
