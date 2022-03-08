f6d2aba8ec4c312a898e5d466bda8804087e7569
Wahyu Agus Arifin - IP <itpohgero@gmail.com>
2022-03-08 14:13:15 +0700

    update login mandiri

--- src/pages/LoginMandiri/index.js
+++ src/pages/LoginMandiri/index.js
@@ -1,27 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { observer } from "mobx-react-lite";
-import { useStore } from "../../utils/useStore";
 import {
   Button,
-  Card,
-  Carousel,
-  Col,
   Form,
   Input,
-  Row,
   Typography,
 } from "antd";
-import { ArrowLeftOutlined, CloseOutlined } from "@ant-design/icons";
-import { useHistory } from "react-router-dom";
-import { FixedTopBar } from "../../component/Header/FixedTopBar";
 import LivinMandiri from "../../assets/images/mandiri.png";
-import TrustLink from "../../assets/images/trustlink.png";
 import { styles } from "./style";
 import { BottomSheet } from "react-spring-bottom-sheet";
 import { SlidesLoginPage } from "../../component/Slides/SlidesLoginPage";
-import Footer from "./components/Footer";
-
 import "react-spring-bottom-sheet/dist/style.css";
+import { PageLogin } from "./../../component/Layouts/PageLogin";
 
 const { Title, Text } = Typography;
 
@@ -48,55 +38,40 @@ export const LoginMandiri = observer(() => {
   };
 
   return (
-    <div style={{ backgroundColor: "#CBDFFF" }}>
-      <FixedTopBar />
-      <div style={styles?.header}>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
+    <PageLogin>
+      <div style={{ color: "#4B4C48" }}>
+        <div style={styles.boxLogo}>
           <div>
-            <ArrowLeftOutlined style={{ fontSize: 16, color: "#71757C" }} />
-          </div>
-          <div>
-            <CloseOutlined style={{ fontSize: 16, color: "#71757C" }} />
-          </div>
-        </div>
-        <div style={{ color: "#4B4C48" }}>
-          <div style={styles.boxLogo}>
-            <div>
-              <div style={{ display: "flex", justifyItems: "center" }}>
-                <img src={LivinMandiri} style={styles.logoMandiri} />
-              </div>
-              <p style={{ fontSize: 18, marginTop: 16 }}>
-                Insert your credentials to start.
-              </p>
+            <div style={{ display: "flex", justifyItems: "center" }}>
+              <img src={LivinMandiri} style={styles.logoMandiri} />
             </div>
+            <p style={{ fontSize: 18, marginTop: 16 }}>
+              Insert your credentials to start.
+            </p>
           </div>
         </div>
-        <Form layout={"vertical"}>
-          <Form.Item name={"email"} label={"Email"}>
-            <Input style={styles.input} placeholder={"email@example.com"} />
-          </Form.Item>
-          <Form.Item name={"password"} label={"Password"}>
-            <Input.Password style={styles.input} />
-          </Form.Item>
-          <div style={styles.forgotPassword}>
-            <Button type="link" style={{ color: "#93969B" }} onClick={() => {}}>
-              Forgot password ?
-            </Button>
-          </div>
-          <Button
-            style={{ backgroundColor: "#B4BCC9", color: "#FFFFFF" }}
-            block
-            size="large"
-          >
-            Connect Account
-          </Button>
-        </Form>
-        <SlidesLoginPage
-          title={`Livin' by Mandiri`}
-          onOpenSheet={onOpenSheet}
-        />
-        <Footer />
       </div>
+      <Form layout={"vertical"}>
+        <Form.Item name={"email"} label={"Email"}>
+          <Input style={styles.input} placeholder={"email@example.com"} />
+        </Form.Item>
+        <Form.Item name={"password"} label={"Password"}>
+          <Input.Password style={styles.input} />
+        </Form.Item>
+        <div style={styles.forgotPassword}>
+          <Button type="link" style={{ color: "#93969B" }} onClick={() => {}}>
+            Forgot password ?
+          </Button>
+        </div>
+        <Button
+          style={{ backgroundColor: "#B4BCC9", color: "#FFFFFF" }}
+          block
+          size="large"
+        >
+          Connect Account
+        </Button>
+      </Form>
+      <SlidesLoginPage title={`Livin' by Mandiri`} onOpenSheet={onOpenSheet} />
       <BottomSheet
         open={open}
         onDismiss={onDismiss}
@@ -104,6 +79,6 @@ export const LoginMandiri = observer(() => {
       >
         My awesome content here
       </BottomSheet>
-    </div>
+    </PageLogin>
   );
 });
