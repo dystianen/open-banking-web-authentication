81742885b8cd1c8023e7d974ce9404d9c5fff02a
Dystian En <distian.enyus@gmail.com>
2022-03-09 11:25:16 +0700

    Feat: Login BRI & Wiring instructions

---
+++ src/pages/LoginBRI/Style.js
@@ -0,0 +1,12 @@
+export const styles = {
+    input: {
+        height: "45px",
+    },
+    forgotPassword: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        marginBottom: "10px",
+        marginTop: "-15px",
+    },
+};
