7efebd3c4432dd71766ace9dcb8648ce7eed3d86
Wahyu Agus Arifin - IP <itpohgero@gmail.com>
2022-03-10 10:48:11 +0700

    update window dimension

--- src/component/Layouts/PageLogin.js
+++ src/component/Layouts/PageLogin.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Card } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faXmark } from "@fortawesome/free-solid-svg-icons";
@@ -7,42 +7,73 @@ import { styles } from "./Style";
 import FooterPageLogin from "./../Footer/FooterPageLogin";
 import { useHistory } from "react-router-dom";
 
+function getWindowDimensions() {
+  const { innerWidth: width } = window;
+  return width;
+}
+
 export const PageLogin = ({ children, linking = true }) => {
   let history = useHistory();
+
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <>
-      <FixedTopBar />
-      <Card style={styles.card}>
-        <div style={styles.container}>
-          {linking && (
-            <div style={styles.nav}>
-              <Button
-                type="link"
-                icon={
-                  <FontAwesomeIcon
-                    icon={faArrowLeft}
-                    style={styles.navButton}
-                  />
-                }
-                onClick={() => {
-                  history.goBack();
-                }}
-              />
-              <Button
-                type="link"
-                icon={
-                  <FontAwesomeIcon icon={faXmark} style={styles.navButton} />
-                }
-                onClick={() => {
-                  history.push("term-condition");
-                }}
-              />
-            </div>
-          )}
-          {children}
-          <FooterPageLogin />
-        </div>
-      </Card>
-    </>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          maxWidth: `${windowDimensions >= 520 ? 520 : windowDimensions}px`,
+        }}
+      >
+        <FixedTopBar />
+        <Card style={styles.card}>
+          <div style={styles.container}>
+            {linking && (
+              <div style={styles.nav}>
+                <Button
+                  type="link"
+                  icon={
+                    <FontAwesomeIcon
+                      icon={faArrowLeft}
+                      style={styles.navButton}
+                    />
+                  }
+                  onClick={() => {
+                    history.goBack();
+                  }}
+                />
+                <Button
+                  type="link"
+                  icon={
+                    <FontAwesomeIcon icon={faXmark} style={styles.navButton} />
+                  }
+                  onClick={() => {
+                    history.push("term-condition");
+                  }}
+                />
+              </div>
+            )}
+            {children}
+            <FooterPageLogin />
+          </div>
+        </Card>
+      </div>
+    </div>
   );
 };
