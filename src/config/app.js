7d368c065387809715c0998a302f04cb49a89774
Farouq <farouq.alfiansyah@gmail.com>
2022-05-10 05:28:11 +0700

    Chore: value apiUrlBilling in appconfig
    TODO : rollback after change in server

--- src/config/app.js
+++ src/config/app.js
@@ -3,7 +3,8 @@ export const appConfig = {
     //TODO: bikin dinamis kalo udah di ganti di servernya
     apiUrl: 'https://cdi-backend.k3s.bangun-kreatif.com',
     urlBilling: process.env.REACT_APP_URL_BILLING,
-    apiUrlBilling: process.env.REACT_APP_API_URL_BILLING,
+    //TODO: bikin dinamis kalo udah di ganti di servernya
+    apiUrlBilling: 'https://cdi-backend.k3s.bangun-kreatif.com',
     mapAPIKey: process.env.REACT_APP_MAP_API_KEY,
     WEBSOCKET_URL: process.env.WEBSOCKET_URL,
     WEBSOCKET_TOKEN: process.env.WEBSOCKET_TOKEN,
