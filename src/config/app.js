97e9b694784bbeeb9f611a12300d4d84a5dbe796
Farouq <farouq.alfiansyah@gmail.com>
2022-05-10 05:18:00 +0700

    Chore: value apiurl in appconfig
    TODO : rollback after change in server

--- src/config/app.js
+++ src/config/app.js
@@ -1,5 +1,7 @@
 export const appConfig = {
-    apiUrl: process.env.REACT_APP_API_URL,
+    // apiUrl: process.env.REACT_APP_API_URL,
+    //TODO: bikin dinamis kalo udah di ganti di servernya
+    apiUrl: 'https://cdi-backend.k3s.bangun-kreatif.com',
     urlBilling: process.env.REACT_APP_URL_BILLING,
     apiUrlBilling: process.env.REACT_APP_API_URL_BILLING,
     mapAPIKey: process.env.REACT_APP_MAP_API_KEY,
