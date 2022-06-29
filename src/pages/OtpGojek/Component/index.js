8b23196112fe7437a36c1325af9c47089659b8a3
Dystian En <distian.enyus@gmail.com>
2022-06-29 12:53:53 +0700

    Fix: Username gojek

--- src/pages/OtpGojek/Component/index.js
+++ src/pages/OtpGojek/Component/index.js
@@ -53,7 +53,7 @@ export const OtpGojek = observer(() => {
             }
             const data = {
                 userId: values.userId,
-                username: 0 + values.phone_number,
+                username: 0 + values.username,
                 customerIdentifier: values.customerIdentifier,
                 customerName: values.customerName,
                 bankCode: values.bankCode,
