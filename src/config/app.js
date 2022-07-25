202b862b3e173b55c0d173bfd53f2fa1480ccdda
Dystian En <distian.enyus@gmail.com>
2022-07-25 18:01:44 +0700

    Feat: new set for app config

--- src/config/app.js
+++ src/config/app.js
@@ -1,10 +1,10 @@
 export const appConfig = {
     // apiUrl: process.env.REACT_APP_API_URL,
     //TODO: bikin dinamis kalo udah di ganti di servernya
-    apiUrl: 'https://cdi-backend.k3s.bangun-kreatif.com',
+    apiUrl: process.env.REACT_APP_API_URL,
     urlBilling: process.env.REACT_APP_URL_BILLING,
     //TODO: bikin dinamis kalo udah di ganti di servernya
-    apiUrlBilling: 'https://cdi-backend.k3s.bangun-kreatif.com',
+    apiUrlBilling: process.env.REACT_APP_API_URL,
     mapAPIKey: process.env.REACT_APP_MAP_API_KEY,
     WEBSOCKET_URL: process.env.WEBSOCKET_URL,
     WEBSOCKET_TOKEN: process.env.WEBSOCKET_TOKEN,
